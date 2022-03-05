d17fe5dbd9dc4fd72c8983b1c93a60e16706dc5e
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2022-03-05 14:42:15 +0200

    Test for different start position

--- Robot/Tests/RobotTests.cs
+++ Robot/Tests/RobotTests.cs
@@ -143,6 +143,34 @@ public class RobotTests
     }
 
     [TestMethod]
+    public void When_Instructions_Specifies_Complex_Movement_With_Multiple_Overlapping_Positions_And_Negative_Starting_Point_Then_Cleaning_Result_Shows_Unique_Cleaned_Positions()
+    {
+        var instructions = new Instructions
+        {
+            AmountOfInstructions = 9,
+            Steps = new List<Tuple<string, int>>()
+            {
+                new Tuple<string, int>("E", 5),
+                new Tuple<string, int>("N", 3),
+                new Tuple<string, int>("W", 2),
+                new Tuple<string, int>("S", 10),
+                new Tuple<string, int>("E", 2),
+                new Tuple<string, int>("N", 10),
+                new Tuple<string, int>("S", 3),
+                new Tuple<string, int>("W", 2),
+                new Tuple<string, int>("W", 3)
+            },
+            StartingPoint = new Point(-10, -10)
+        };
+
+        var robot = new Robot(instructions);
+
+        var cleaningResult = robot.Clean();
+
+        cleaningResult.Should().Be(28);
+    }
+
+    [TestMethod]
     public void When_Amount_Of_Instructions_Are_Less_Than_Steps_Then_Only_Specified_Steps_Are_Executed()
     {
         var instructions = new Instructions
